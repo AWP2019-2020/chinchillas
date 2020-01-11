5b7d2c12e92cdc62fd80f50ab2b34d8c7fdda2b7
Catalin Sasu <sasucatalingabriel@gmail.com>
2020-01-11 14:57:58 +0200

    Added user_profile page

--- shop/admin.py
+++ shop/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from shop import models
-from shop.models import Product, Category, Review
+from shop.models import Product, Category, Review, UserProfile, Country
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Review)
+admin.site.register(UserProfile)
+admin.site.register(Country)
--- shop/views.py
+++ shop/views.py
@@ -4,11 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
-    View, TemplateView, CreateView,
+    View, TemplateView, CreateView, DetailView
 )
 
 from shop.forms import ReviewForm
-from shop.models import User, Product, Category, Review
+from shop.models import User, Product, Category, Review, UserProfile
 from django.urls import reverse, reverse_lazy
 
 
@@ -82,3 +82,12 @@ def review_create(request, pk):
                 **form.cleaned_data
             )
             return redirect(reverse_lazy("product_detail", kwargs={"pk": pk}))
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    template_name = 'user_profile.html'
+    context_object_name = 'userprofile'
+
+    def get_object(self):
+        user = User.objects.get(id=self.kwargs['pk'])
+        userprofile = user.profile.first()
+        return userprofile
