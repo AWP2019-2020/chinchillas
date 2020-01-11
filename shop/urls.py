55c51ee2218cce20296645b9f6a572cc03a4aa38
Andrei Alexandru Zota <Alexandru_Zota@Dell.com>
2020-01-11 12:22:26 +0200

    Finished products

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import product_detail, category_detail
+from .views import product_detail, category_detail, review_create
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<int:pk>', product_detail, name='product_detail'),
     path('category/<int:pk>', category_detail, name='category_detail'),
-
+    path('product/<int:pk>/review/create', review_create, name='review_create'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+from django.urls import reverse_lazy
+
+from shop.forms import ReviewForm
 from shop.models import Product, Category, Review
 
 
@@ -22,3 +26,17 @@ def category_detail(request, pk):
 def review_detail(request, pk):
     review_ = Review.objects.get(id=pk)
     return render(request, "review__detail.html", {"review_": review_})
+
+
+@login_required
+def review_create(request, pk):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            product = Product.objects.get(id=pk)
+            Review.objects.create(
+                created_by=request.user,
+                post=product,
+                **form.cleaned_data
+            )
+            return redirect(reverse_lazy("product_detail", kwargs={"pk": pk}))
